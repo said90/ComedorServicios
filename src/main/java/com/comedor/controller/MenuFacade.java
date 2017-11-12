ac8e3c57bf24a11f75ecec23b09b1b574a6bf424
bgarcia95 <bgarcia95@NERV>
2017-11-12 17:03:54 -0600

    modelado menu

---
+++ src/main/java/com/comedor/controller/MenuFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.comedor.controller;
+
+import com.comedor.model.Menu;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author bgarcia95
+ */
+@Stateless
+public class MenuFacade extends AbstractFacade<Menu> implements MenuFacadeLocal {
+
+    @PersistenceContext(unitName = "com.mycompany_Comedor_war_1.0-SNAPSHOTPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public MenuFacade() {
+        super(Menu.class);
+    }
+    
+}
---
+++ src/main/java/com/comedor/model/Menu.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.comedor.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author bgarcia95
+ */
+@Entity
+@Table(name="menu")
+public class Menu implements Serializable {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int idmenu;
+    
+    @JoinColumn(name = "idPlatillo", referencedColumnName = "idPlatillo")
+    @ManyToOne
+    private int idPlatillo;
+    
+    @Column(name="precio")
+    private float precio;
+
+    public int getIdmenu() {
+        return idmenu;
+    }
+
+    public void setIdmenu(int idmenu) {
+        this.idmenu = idmenu;
+    }
+
+    public int getIdPlatillo() {
+        return idPlatillo;
+    }
+
+    public void setIdPlatillo(int idPlatillo) {
+        this.idPlatillo = idPlatillo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    
+    
+}
